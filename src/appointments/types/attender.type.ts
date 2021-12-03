360832f2fd4c998c5978b1d8a32eb38434dfade6
bitsatebyte <hariveturi@gmail.com>
2021-12-04 03:31:42 +0530

    ADD: appointment types

---
+++ src/appointments/types/attender.type.ts
@@ -0,0 +1,6 @@
+export type Attender = {
+  name: string;
+  age: number;
+  description: string;
+  job: string;
+};
---
+++ src/appointments/types/establishment.type.ts
@@ -0,0 +1,5 @@
+export type Establishment = {
+  name: string;
+  type: 'hospital' | 'gym';
+  description: string;
+};
