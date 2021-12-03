82934bca61de65f164ef750156a3e0007dc1616a
bitsatebyte <hariveturi@gmail.com>
2021-12-04 00:37:57 +0530

    CLN: move exception to its folder

--- src/tasks/budget.exceeded.exception.ts
+++
@@ -1,7 +0,0 @@
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-export class BudgetExceededException extends HttpException {
-  constructor() {
-    super('BudgetExceeded', HttpStatus.FORBIDDEN);
-  }
-}
