8e65980980176aeb922748994fad07cdcde9b71a
bitsatebyte <hariveturi@gmail.com>
2021-12-04 15:02:04 +0530

    ENH: add cron job to schedule appointments

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AppointmentsModule } from 'src/appointments/appointments.module';
 import { OrdersModule } from 'src/orders/orders.module';
 import { SubscriptionsModule } from 'src/subscriptions/subscriptions.module';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [SubscriptionsModule, OrdersModule],
+  imports: [SubscriptionsModule, OrdersModule, AppointmentsModule],
   providers: [TasksService],
 })
 export class TasksModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,17 +4,22 @@ import { CreateOrderDto } from 'src/orders/dto/create-order.dto';
 import { OrdersService } from 'src/orders/orders.service';
 import { Subscription } from 'src/subscriptions/entities/subscription.entity';
 import { SubscriptionsService } from 'src/subscriptions/subscriptions.service';
-import { Item } from 'src/subscriptions/types/items';
+import { Item, Items } from 'src/subscriptions/types/items.type';
 import { BudgetExceededException } from './exceptions/budget.exceeded.exception';
 import { SubscriptionExpiredException } from './exceptions/subscription.expired.exception';
 import { SubscriptionPausedException } from './exceptions/subscription.paused.exception';
-import { OrderStatus } from 'src/orders/types/order.status';
+import { OrderStatus } from 'src/orders/types/order.status.type';
+import { AppointmentsService } from 'src/appointments/appointments.service';
+import { CreateAppointmentDto } from 'src/appointments/dto/create-appointment.dto';
+import { AppointmentItem } from 'src/appointments/types/appointment.type';
+import { Establishment } from 'src/appointments/types/establishment.type';
 
 @Injectable()
 export class TasksService {
   constructor(
     private readonly ordersService: OrdersService,
     private readonly subscriptionsService: SubscriptionsService,
+    private readonly appointmentsService: AppointmentsService,
   ) {}
 
   private readonly logger = new Logger(TasksService.name);
@@ -48,9 +53,38 @@ export class TasksService {
     });
   }
 
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async handleCreateAppointmentCron() {
+    const subscriptions = await this.subscriptionsService.findAll();
+    if (!subscriptions.length) return;
+    subscriptions.forEach(async (subscription) => {
+      const { subscriptionType } = subscription;
+      this.handleSubscriptionExceptions(subscription);
+      if (subscriptionType === 'appointment') {
+        const appointmentDto = this.prepareAppointmentEntity(subscription);
+        // log appointment placing event
+        this.logger.log(`booking the appointment`);
+        const appointment = await this.appointmentsService.create(
+          appointmentDto,
+        );
+        // calculate remaining budget
+        const budget = subscription.budget - appointmentDto.amount;
+        // update the budget and set the custom order to null
+        await this.subscriptionsService.update(subscription.id, {
+          budget,
+          custom: null,
+        });
+        // emit appointment created event to frontend
+        // this.socket.emit('appointment.create', { appointment });
+      }
+    });
+  }
+
   prepareOrderEntity(subscription: Subscription): CreateOrderDto {
-    const { customer, custom } = subscription;
-    const defaultOrder = subscription.default;
+    let { customer, custom } = subscription;
+    let defaultOrder = subscription.default;
+    custom = custom as Items;
+    defaultOrder = defaultOrder as Items;
     const restaurant = defaultOrder.restaurant;
     const items: Item[] = [];
     if (!custom) {
@@ -62,14 +96,40 @@ export class TasksService {
         items.push(custom.list[key]);
       }
     }
-    const order: CreateOrderDto = {
+    const order = {
       customer,
       items,
       restaurant,
       total: 300,
-      status: 'processing',
+      status: 'processing' as OrderStatus,
     };
+
     return order;
+  }
+
+  prepareAppointmentEntity(subscription: Subscription): CreateAppointmentDto {
+    let { customer, custom, time } = subscription;
+    let defaultAppointment = subscription.default;
+    custom = custom as AppointmentItem;
+    defaultAppointment = defaultAppointment as AppointmentItem;
+    let establishment: Establishment, amount: number;
+    if (!custom) {
+      establishment = defaultAppointment.establishment;
+      amount = defaultAppointment.total;
+    } else {
+      establishment = custom.establishment;
+      amount = custom.total;
+    }
+    const appointment: CreateAppointmentDto = {
+      customer,
+      establishment,
+      time,
+      amount,
+      date: new Date(),
+      duration: 60,
+    };
+
+    return appointment;
   }
 
   handleSubscriptionExceptions(options: Subscription) {
@@ -78,10 +138,10 @@ export class TasksService {
     const today = new Date().getTime();
     const hasSubscriptionExpired = subscriptionEndDate <= today;
     // check if subscription has expired
-    if (hasSubscriptionExpired || subscriber.status === 'cancelled')
+    if (hasSubscriptionExpired || subscriber === 'cancelled')
       throw new SubscriptionExpiredException();
     // check if subscription is on pause
-    if (subscriber.status === 'paused') throw new SubscriptionPausedException();
+    if (subscriber === 'paused') throw new SubscriptionPausedException();
     // check if budget has exceeded
     if (custom && custom.total > budget) throw new BudgetExceededException();
   }
